ef64dd13493a9f5a618a65d85b4bf828c4300dbb
cardoso <cardososilvamateus@gmail.com>
2019-10-23 04:25:36 -0300

    arquivo

---
+++ teste.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta charset="utf-8">
+<head>
+	<title>WHITE PAGE</title>
+</head>
+<body>
+
+
+<div id="dale" align="center" width="50%" color:blue;>
+	
+
+<form method="post" action="$mec">
+
+<label for="name">Nome: </label>	
+<input type="text" name="nome" placeholder="seu nome" required>
+<label for="senha"></label>
+<input type="text" name="senha" id="senha" placeholder="senha" required>
+<input type="submit" name="">
+
+
+
+</form>
+
+
+
+
+</div>
+
+
+
+<?php
+
+
+ $mec;
+
+echo $mec;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+</html>
